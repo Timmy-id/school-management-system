b7560b579fec187811565a724965a83535eaa6c0
Oluwatimilehin <oluwatimilehin.id@gmail.com>
2020-07-24 21:43:34 +0100

    created the lectures models and made some updates

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('apps.users.urls')),
     path('api/v1/certificates/', include('apps.certificates.urls')),
-    path('api/v1/waitlist/', include('apps.waitlist.urls')),
+    path('api/v1/lectures/', include('apps.lectures.urls')),
 ]
